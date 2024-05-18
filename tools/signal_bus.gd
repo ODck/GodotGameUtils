f3dc37983b93e23c6c148966d1bb448b1782ea85
ODck <alvaro.per.rui@gmail.com>
2024-05-19 01:01:12 +0200

    signal bus now properly unpacks the argument array

--- tools/signal_bus.gd
+++ tools/signal_bus.gd
@@ -21,8 +21,52 @@ func unsubscribe(signal_name: String, callable: Callable):
 		Logger.error("Signal %s unsubscribe failed, subscribe the signal first" % signal_name)
 
 ## Publishes a signal with an optional array of arguments.
-func publish(signal_name: String, args: Array = []):
+func _legacy_publish(signal_name: String, args: Array = []):
 	if has_user_signal(signal_name):
 		emit_signal(signal_name, args)
 	else:
 		Logger.warning("Signal %s published without subscribers" % signal_name)
+
+# Publishes a signal with an optional array of arguments
+func publish(signal_name: String, args: Array = []):
+	if has_user_signal(signal_name):
+		for subscriber in get_signal_connection_list(signal_name):
+			var callable = subscriber.callable;
+			var expected_arg_count = get_method_arg_count(callable)
+			if expected_arg_count != args.size():
+				Logger.warning("Signal %s expected %d arguments, but got %d for callable %s" % [signal_name, expected_arg_count, args.size(), callable])
+
+
+		match args.size():
+			0:
+				emit_signal(signal_name)
+			1:
+				emit_signal(signal_name, args[0])
+			2:
+				emit_signal(signal_name, args[0], args[1])
+			3:
+				emit_signal(signal_name, args[0], args[1], args[2])
+			4:
+				emit_signal(signal_name, args[0], args[1], args[2], args[3])
+			5:
+				emit_signal(signal_name, args[0], args[1], args[2], args[3], args[4])
+			6:
+				emit_signal(signal_name, args[0], args[1], args[2], args[3], args[4], args[5])
+			7:
+				emit_signal(signal_name, args[0], args[1], args[2], args[3], args[4], args[5], args[6])
+			8:
+				emit_signal(signal_name, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
+			# Add more cases if you expect more arguments
+			_:
+				emit_signal(signal_name, args)
+	else:
+		Logger.warning("Signal %s published without subscribers" % signal_name)
+
+
+# Get the number of arguments expected by a callable
+func get_method_arg_count(m: Callable):
+	for f in m.get_object().get_method_list():
+		var fn = get(f.name)
+		if m.get_method() == f.name:
+			return f.args.size()
+	return 0
